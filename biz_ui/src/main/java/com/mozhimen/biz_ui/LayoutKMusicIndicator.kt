da3cfb5b1c358564da5801ea019cd9b06a7ca3fb
mozhimen <mozhimen@163.com>
2022-11-07 03:25:59 +0800

    1107

--- biz_ui/src/main/java/com/mozhimen/biz_ui/LayoutKMusicIndicator.kt
+++ biz_ui/src/main/java/com/mozhimen/biz_ui/LayoutKMusicIndicator.kt
@@ -4,11 +4,11 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.mozhimen.basick.basek.BaseKLayoutRelative
-import com.mozhimen.biz_ui.R
 
 /**
  * @ClassName ViewKMusicIndicator
@@ -22,6 +22,7 @@ class LayoutKMusicIndicator @JvmOverloads constructor(context: Context, attrs: A
 
     private lateinit var _imgViewIndicator: ImageView
     private lateinit var _pagerDisc: ViewPager
+    private lateinit var _musicPagerAdapter: MusicPagerAdapter
 
     init {
         initView()
@@ -32,6 +33,7 @@ class LayoutKMusicIndicator @JvmOverloads constructor(context: Context, attrs: A
         _imgViewIndicator = container.findViewById(R.id.layoutk_music_indicator_indicator)
         _pagerDisc = container.findViewById(R.id.layoutk_music_indicator_page)
         _pagerDisc.apply { overScrollMode = View.OVER_SCROLL_NEVER }
+        _musicPagerAdapter = MusicPagerAdapter()
     }
 
     override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
@@ -46,7 +48,18 @@ class LayoutKMusicIndicator @JvmOverloads constructor(context: Context, attrs: A
 
     }
 
-    class MusicPagerAdapter(context: Context) : PagerAdapter() {
+    class MusicPagerAdapter(
+        private val _context: Context,
+        private val _imgUrls: ArrayList<String>
+    ) : PagerAdapter() {
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            val rootView = LayoutInflater.from(_context).inflate(R.layout.layoutk_music_indicator_item, null)
+            val imgView: ImageView = rootView.findViewById(R.id.layoutk_music_indicator_item_img)
+            container.addView(rootView)
+
+            return
+        }
+
         override fun getCount(): Int {
 
         }
